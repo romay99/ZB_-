3d88b49e7e77011fae8b6f3027221e4206f2d691
SooBin_Lee <i_romay@naver.com>
2024-12-16 17:56:37 +0900

    first commit

---
+++ src/main/java/com/zerobase/Store_Table_Reservation/StoreTableReservationApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.Store_Table_Reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreTableReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreTableReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/Store_Table_Reservation/StoreTableReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.Store_Table_Reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreTableReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
